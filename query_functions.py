add5155d43628274b6c81b283e020df07c56f4a6
Nick Miller <miller.nick.c@gmail.com>
2024-08-15 19:50:52 +0000

    reorganized a lot of query code

--- query_functions.py
+++ query_functions.py
@@ -10,19 +10,6 @@ from typing import List, Optional, Dict, Any, Callable, Generator
 import logging
 import time
 
-def format_messages(
-    prompt: str, 
-    system_instructions: str, 
-    history_messages: List[Dict[str, Any]]
-) -> List[Dict[str, str]]:
-    """Format the messages for the OpenAI API."""
-    formatted_messages = []
-    if system_instructions:
-        formatted_messages.append({"role": "system", "content": system_instructions})
-    formatted_messages.extend(history_messages)
-    formatted_messages.append({"role": "user", "content": prompt})
-    return formatted_messages
-
 def groq_query(
     prompt: str, 
     system_instructions: str = None, 
@@ -30,12 +17,28 @@ def groq_query(
     model_choice: str = "llama3.1-70b"
 ) -> str:
     """
-    Query the Groq API for a response based on the provided prompt and history.
+        Executes a query using the Groq model to generate a response based on the provided prompt and chat history.
+
+        Parameters:
+        - prompt (str): The user prompt that guides the model's response.
+        - system_instructions (str, optional): Instructions for the model to follow while generating the response.
+        - history_messages (List[Dict[str, Any]], optional): A list of previous messages in the chat history to provide context.
+        - model_choice (str, optional): The specific model to use for generating the response. Defaults to "llama3.1-70b".
+
+        Returns:
+        - str: The generated response from the Groq model.
+
+        This function formats the input messages, calls the Groq model, and handles any exceptions that may occur during the process.
     """
     if history_messages is None:
         history_messages = []
 
-    formatted_messages = format_messages(prompt, system_instructions, history_messages)
+    formatted_messages = _format_messages(
+        prompt, 
+        system_instructions, 
+        history_messages
+    )
+
     try:
         response = groq_text_response(
             prompt, 
@@ -48,7 +51,54 @@ def groq_query(
         logging.error(f"Error: {e}")
         return "Sorry, I don't understand."
 
-def create_vectordb_query(
+def _format_messages(
+    prompt: str, 
+    system_instructions: str, 
+    history_messages: List[Dict[str, Any]]
+) -> List[Dict[str, str]]:
+    """Format the messages for the OpenAI API."""
+    formatted_messages = []
+    if system_instructions:
+        formatted_messages.append({
+            "role": "system", 
+            "content": system_instructions
+        })
+    formatted_messages.extend(history_messages)
+    # formatted_messages.append({
+    #     "role": "user", 
+    #     "content": prompt
+    # })
+
+    return formatted_messages
+
+def search_vector_db(
+    question: str,
+    file_path: str,
+    history_messages: List[Dict[str, str]], 
+    similarity_threshold: float = 0.4,
+    filter_limit: int = 15,
+    max_similarity_delta: float = 0.075,
+) -> List[Dict[str, Any]]:
+    start_time = time.time()
+    
+    vectordb_query = _create_vectordb_query(question, history_messages)
+    vectordb_end_time = time.time()
+    logging.info(f"\nVectordb duration: {vectordb_end_time - start_time} seconds\n")
+
+    similar_chunks = _retrieve_similar_chunks(
+        file_path, 
+        vectordb_query, 
+        similarity_threshold=similarity_threshold,
+        filter_limit=filter_limit,
+        max_similarity_delta=max_similarity_delta,
+    )
+    
+    similar_chunks_end_time = time.time()
+    logging.info(f"\nSimilar chunk retrieval duration: {similar_chunks_end_time - vectordb_end_time} seconds\n")
+
+    return similar_chunks
+
+def _create_vectordb_query(
     question: str,
     history_messages: List[Dict[str, str]], 
 ) -> str:
@@ -73,7 +123,7 @@ def create_vectordb_query(
 
     return vectordb_query
 
-def retrieve_similar_chunks(
+def _retrieve_similar_chunks(
     file_path: str,
     query: str, 
     similarity_threshold: float,
@@ -82,6 +132,29 @@ def retrieve_similar_chunks(
     embedding_function: Callable = create_openai_embedding, 
     model_choice: str = "text-embedding-3-large"
 ) -> List[Dict[str, Any]]:
+    """
+        This function retrieves and identifies chunks of text that are similar to a given query based on their embeddings.
+
+        Parameters:
+        - file_path (str): The path to the file containing the data from which chunks will be extracted.
+        - query (str): The input query string for which similar chunks are to be found.
+        - similarity_threshold (float): The minimum similarity score for a chunk to be considered similar.
+        - filter_limit (int): The maximum number of similar chunks to return.
+        - max_similarity_delta (float): The maximum allowed difference in similarity between the most similar chunk and the others to be included in the results.
+        - embedding_function (Callable): A callable function that generates embeddings for the given text. Defaults to create_openai_embedding.
+        - model_choice (str): The model to be used for generating embeddings. Defaults to "text-embedding-3-large".
+
+        Returns:
+        - List[Dict[str, Any]]: A list of dictionaries containing the similar chunks, each with an added 'similarity' key indicating the similarity score.
+
+        Functionality:
+            1. Extracts data from the specified file using the retrieve_file function.
+            2. Computes embeddings for the query and compares them against the extracted chunks using the find_similar_chunks function.
+            3. Logs the number of similar chunks retrieved and any warnings if no similar chunks are found.
+            4. Returns the list of similar chunks that meet the similarity criteria.
+
+        This function is essential for enabling the system to respond to user queries by identifying relevant information based on semantic similarity.
+    """
     extracted_list = retrieve_file(file_path)
     if not extracted_list:
         logging.error(f"No data extracted from file: {file_path}")
@@ -105,7 +178,62 @@ def retrieve_similar_chunks(
 
     return similar_chunks
 
-def revise_query(
+def query_data(
+    question: str,
+    similar_chunks: List[Dict[str, Any]],
+    history_messages: List[Dict[str, str]], 
+) -> str:
+    """
+        Queries the data based on the provided question and history messages.
+
+        This function serves as the main entry point for querying the vector database and retrieving relevant 
+        information based on the user's question. It performs the following steps:
+
+        1. **History Message Preparation**: It takes the last few messages from the chat history and 
+        prepends a system message to provide context for the query.
+
+        2. **Vector Database Query Creation**: It constructs a query specifically tailored for the vector 
+        database using the provided question and the prepared history messages. This helps in understanding 
+        the context and intent behind the user's question.
+
+        3. **Retrieving Similar Chunks**: The function then calls another function to retrieve similar chunks 
+        of text from the specified file based on the generated vector database query. This step is crucial 
+        for finding relevant information that can help answer the user's question.
+
+        4. **Query Revision**: After retrieving similar chunks, it revises the original question to enhance 
+        its clarity and relevance based on the chat history. This ensures that the query is as effective as 
+        possible in eliciting a useful response.
+
+        5. **Generating the Answer**: Finally, it generates an answer using the revised query, the history 
+        messages, and the similar chunks retrieved. The function returns both the generated answer and the 
+        list of similar chunks for further processing or display.
+
+        Parameters:
+        - question (str): The question to be asked.
+        - file_path (str): The path to the file containing the data.
+        - history_messages (List[Dict[str, str]]): A list of dictionaries representing the chat history, 
+        where each dictionary contains the role (user or assistant) and the content of the message.
+
+        Returns:
+        - Tuple[str, List[Dict[str, str]]]: A tuple containing the generated answer as a string and 
+        a list of similar chunks retrieved from the data source.
+    """
+    print(f"\n\nDQ HISTORY MESSAGES: {history_messages}\n\n")
+
+    start_time = time.time()
+    
+    new_query = _revise_query(question, history_messages)
+
+    new_query_end_time = time.time()
+    logging.info(f"\nNew query duration: {new_query_end_time - start_time} seconds\n")
+
+    return _generate_answer(
+        new_query, 
+        history_messages, 
+        similar_chunks
+    )
+
+def _revise_query(
     question: str,
     history_messages: List[Dict[str, str]], 
 ) -> str:
@@ -132,7 +260,7 @@ def revise_query(
 
     return new_query
 
-def generate_answer(
+def _generate_answer(
     query: str,
     history_messages: List[Dict[str, str]],
     similar_chunks: List[Dict[str, Any]],
@@ -186,7 +314,6 @@ def generate_answer(
     Book: *{title}*
     Chapter: {chapter}
     Author: {author}
-    Publisher: {publisher}
 
     Text: {text}
     """
@@ -223,86 +350,6 @@ def generate_answer(
         ],
     )
 
-def search_vector_db(
-    question: str,
-    file_path: str,
-    history_messages: List[Dict[str, str]], 
-    similarity_threshold: float = 0.4,
-    filter_limit: int = 15,
-    max_similarity_delta: float = 0.075,
-) -> str:
-    start_time = time.time()
-    
-    vectordb_query = create_vectordb_query(question, history_messages)
-    vectordb_end_time = time.time()
-    logging.info(f"\nVectordb duration: {vectordb_end_time - start_time} seconds\n")
-
-    similar_chunks = retrieve_similar_chunks(
-        file_path, 
-        vectordb_query, 
-        similarity_threshold=similarity_threshold,
-        filter_limit=filter_limit,
-        max_similarity_delta=max_similarity_delta,
-    )
-    
-    similar_chunks_end_time = time.time()
-    logging.info(f"\nSimilar chunk retrieval duration: {similar_chunks_end_time - vectordb_end_time} seconds\n")
-
-    return similar_chunks
-
-def query_data(
-    question: str,
-    similar_chunks: List[Dict[str, Any]],
-    history_messages: List[Dict[str, str]], 
-) -> str:
-    """
-        Queries the data based on the provided question and history messages.
-
-        This function serves as the main entry point for querying the vector database and retrieving relevant 
-        information based on the user's question. It performs the following steps:
-
-        1. **History Message Preparation**: It takes the last few messages from the chat history and 
-        prepends a system message to provide context for the query.
-
-        2. **Vector Database Query Creation**: It constructs a query specifically tailored for the vector 
-        database using the provided question and the prepared history messages. This helps in understanding 
-        the context and intent behind the user's question.
-
-        3. **Retrieving Similar Chunks**: The function then calls another function to retrieve similar chunks 
-        of text from the specified file based on the generated vector database query. This step is crucial 
-        for finding relevant information that can help answer the user's question.
-
-        4. **Query Revision**: After retrieving similar chunks, it revises the original question to enhance 
-        its clarity and relevance based on the chat history. This ensures that the query is as effective as 
-        possible in eliciting a useful response.
-
-        5. **Generating the Answer**: Finally, it generates an answer using the revised query, the history 
-        messages, and the similar chunks retrieved. The function returns both the generated answer and the 
-        list of similar chunks for further processing or display.
-
-        Parameters:
-        - question (str): The question to be asked.
-        - file_path (str): The path to the file containing the data.
-        - history_messages (List[Dict[str, str]]): A list of dictionaries representing the chat history, 
-        where each dictionary contains the role (user or assistant) and the content of the message.
-
-        Returns:
-        - Tuple[str, List[Dict[str, str]]]: A tuple containing the generated answer as a string and 
-        a list of similar chunks retrieved from the data source.
-    """
-
-    start_time = time.time()
-    
-    new_query = revise_query(question, history_messages)
-
-    new_query_end_time = time.time()
-    logging.info(f"\nNew query duration: {new_query_end_time - start_time} seconds\n")
-
-    return generate_answer(
-        new_query, 
-        history_messages, 
-        similar_chunks
-    )
 
 if __name__ == "__main__":
     file_path = "./extracted_documents/all_books_paragraphs.json"
